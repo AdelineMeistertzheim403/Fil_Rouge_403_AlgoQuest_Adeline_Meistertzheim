a6151af5f1e0424759a24a08b7d5141f0aedf2fd
AdelineMeisterFormatrice <meistertzheimadeline@gmail.com>
2025-09-14 17:36:36 +0200

    modif titre page accueil

--- algoquest-mobile/app/index.tsx
+++ algoquest-mobile/app/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Text style={{ fontSize: 20, marginBottom: 20 }}>
-                Bienvenue sur AlgoQuest 🚀
+                Bienvenue sur AlgoQuest
             </Text>
             <Button title="Connexion" onPress={() => router.push("/(auth)/login")} />
             <Button title="Inscription" onPress={() => router.push("/(auth)/register")} />
