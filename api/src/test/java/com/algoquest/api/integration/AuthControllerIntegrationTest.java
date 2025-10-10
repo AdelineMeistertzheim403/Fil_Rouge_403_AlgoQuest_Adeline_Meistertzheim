3e58ba4e4bd4ffa7a568d70b9449042c1bf18f81
AdelineMeisterFormatrice <meistertzheimadeline@gmail.com>
2025-10-11 00:23:21 +0200

    Fix CI: use real MongoDB instead of embedded in GitHub Actions

--- api/src/test/java/com/algoquest/api/integration/AuthControllerIntegrationTest.java
+++ api/src/test/java/com/algoquest/api/integration/AuthControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.algoquest.api.config.EmbeddedMongoTestConfig;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test-ci")
 @Import({EmbeddedMongoTestConfig.class, TestSecurityConfig.class})
 public class AuthControllerIntegrationTest {
 
