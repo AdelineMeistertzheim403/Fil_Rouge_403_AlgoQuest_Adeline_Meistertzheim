ad24ed1fa360fa90c7f01be82406fe445c062cac
AdelineMeisterzheim403 <meistertzheimadeline@gmail.com>
2025-10-24 16:14:17 +0200

    ajout role mapping

--- algoquest-mobile/src/api/client.ts
+++ algoquest-mobile/src/api/client.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const api = axios.create({
-    baseURL: "http://10.0.2.2:8080/api/v1",
-    //baseURL: "https://apialgoquest.adelinemeistertzheim.fr/api/v1",
+    //baseURL: "http://10.0.2.2:8080/api/v1",
+    baseURL: "https://apialgoquest.adelinemeistertzheim.fr/api/v1",
     //baseURL: "http://localhost:8080/api/v1",
 });
 
